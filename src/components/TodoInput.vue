0976fe8a97ff37713dec274e181a1f0c664e00ac
hong-p <iyhong0721@gmail.com>
2022-03-09 16:42:27 +0900

    Todo App - 프로젝트 구현  - TodoInput 컴포넌트 할일 저장 구현

--- src/components/TodoInput.vue
+++ src/components/TodoInput.vue
@@ -1,9 +1,26 @@
 <template>
-  <div>input</div>
+  <div>
+    <input type="text" v-model="newTodoItem" @keydown.enter="addTodo" />
+    <button @click="addTodo">add</button>
+  </div>
 </template>
 
 <script>
-export default {};
+export default {
+  data() {
+    return {
+      newTodoItem: '',
+    }
+  },
+  methods: {
+    addTodo() {
+      console.log(this.newTodoItem)
+      // 저장로직 수행
+      localStorage.setItem(this.newTodoItem, this.newTodoItem)
+      this.newTodoItem = ''
+    },
+  },
+}
 </script>
 
 <style></style>
