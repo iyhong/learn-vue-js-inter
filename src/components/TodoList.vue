e6ec90a50694c4d3d197adb9e4192d93e3909687
hong-p <iyhong0721@gmail.com>
2022-03-10 17:34:29 +0900

    Todo App - 애플리케이션 구조 개선하기  - vue 트랜지션 구현(애니메이션 효과)

--- src/components/TodoList.vue
+++ src/components/TodoList.vue
@@ -1,6 +1,6 @@
 <template>
   <div>
-    <ul>
+    <TransitionGroup name="list" tag="ul">
       <li
         class="shadow"
         v-for="(todoItem, index) in propsdata"
@@ -18,7 +18,7 @@
           <i class="fa-solid fa-trash-can" />
         </span>
       </li>
-    </ul>
+    </TransitionGroup>
   </div>
 </template>
 
@@ -74,5 +74,19 @@ li {
 .removeBtn {
   margin-left: auto;
   color: #de4343;
+}
+
+/* 리스트 아이템 트랜지션 효과 */
+.list-item {
+  display: inline-block;
+  margin-right: 10px;
+}
+.list-enter-active,
+.list-leave-active {
+  transition: all 1s;
+}
+.list-enter, .list-leave-to /* .list-leave-active below version 2.1.8 */ {
+  opacity: 0;
+  transform: translateY(30px);
 }
 </style>
